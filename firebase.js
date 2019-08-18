e21f33c3f0ddd5ca451215c64282a06867536344
Akin Aguda <lordtitan7@gmail.com>
2019-08-18 18:15:40 +0100

    Started become a member form

---
+++ firebase.js
@@ -0,0 +1,7 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyAxKjLcn5dYAIU-9clM5UaQ-h5xevNs4Hs",
+  authDomain: "dsc-unilag.firebaseapp.com",
+  projectId: "dsc-unilag"
+});
+
+// var db = firebase.firestore();
